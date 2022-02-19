91ec23fbf53643860b8d6f2a250bb399a6cafc58
Abdullah <87931418+A-Washtan@users.noreply.github.com>
2022-02-19 22:54:24 +0300

    updit scrool

---
+++ main.js
@@ -0,0 +1,24 @@
+
+
+let nums = document.querySelectorAll(".stats .number");
+let section = document.querySelector(".box");
+let started = false; // Function Started ? No
+
+window.onscroll = function () {
+    if (window.scrollY >= section.offsetTop) {
+        if (!started) {
+            nums.forEach((num) => startCount(num));
+        }
+        started = true;
+    }
+};
+
+function startCount(el) {
+    let goal = el.dataset.goal;
+    let count = setInterval(() => {
+        el.textContent++;
+        if (el.textContent == goal) {
+            clearInterval(count);
+        }
+    }, 5000 / goal);
+}
